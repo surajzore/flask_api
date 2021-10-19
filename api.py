59107106ac2592a2da465c83bd3e2ee49bcd3f6f
suraj550 <80310603+suraj550@users.noreply.github.com>
2021-10-19 12:23:18 +0530

    decode token

--- api.py
+++ api.py
@@ -42,6 +42,7 @@ def token_required(f):
             return jsonify({'message': 'Token is missing!'}), 401
 
         try:
+            # decode token
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256", ])
             current_user = User.query.filter_by(public_id=data['public_id']).first()
         except:
